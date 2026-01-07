8eabf507e980b574d04823d281eb96b0db188b4a
Muhammad Jawad <112897469+muhammadJawadm@users.noreply.github.com>
2026-01-07 11:31:13 +0500

    make a small modification in community service

--- src/services/communityServices.ts
+++ src/services/communityServices.ts
@@ -1,14 +1,14 @@
 import { supabase } from "../lib/supabase";
 
 export interface Community {
-  id: number;               // bigint → number in TS
-  catagory_id: number;      // foreign key
-  title: string;
-  description: string;
-  created_at: string;       // timestamp with time zone → ISO string
+    id: number;               // bigint → number in TS
+    catagory_id: number;      // foreign key
+    title: string;
+    description: string;
+    created_at: string;       // timestamp with time zone → ISO string
 }
 
-export const fetchCommunities = async(): Promise<Community[]> => {
+export const fetchCommunities = async (): Promise<Community[]> => {
     let { data, error } = await supabase.from("community").select("*");
     if (error) {
         console.log("Error fetching communities:", error);
@@ -17,7 +17,7 @@ export const fetchCommunities = async(): Promise<Community[]> => {
     return data as Community[];
 };
 
-export const deleteCommunity = async(id: number): Promise<boolean> => {
+export const deleteCommunity = async (id: number): Promise<boolean> => {
     let { error } = await supabase.from("community").delete().eq("id", id);
     if (error) {
         console.log("Error deleting community:", error);
@@ -26,8 +26,8 @@ export const deleteCommunity = async(id: number): Promise<boolean> => {
     return true;
 };
 
-export const addCommunity = async(community: Omit<Community, 'id' | 'created_at'>): Promise<Community | null> => {
-    let { data, error } = await supabase.from("community").insert(community).select().single();
+export const addCommunity = async (community: Omit<Community, 'id' | 'created_at'>): Promise<Community | null> => {
+    let { data, error } = await supabase.from("community").insert({ community, is_heroz: true, }).select().single();
     if (error) {
         console.log("Error adding community:", error);
         return null;
@@ -35,7 +35,7 @@ export const addCommunity = async(community: Omit<Community, 'id' | 'created_at'
     return data;
 };
 
-export const updateCommunity = async(id: number, updatedFields: Partial<Omit<Community, 'id' | 'created_at'>>): Promise<Community | null> => {
+export const updateCommunity = async (id: number, updatedFields: Partial<Omit<Community, 'id' | 'created_at'>>): Promise<Community | null> => {
     let { data, error } = await supabase.from("community").update(updatedFields).eq("id", id).select().single();
     if (error) {
         console.log("Error updating community:", error);
